d4996894f722b42b1424704351188cfd4ca3716e
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-06-18 14:57:41 -0400

    added beep beep alert

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+// Makes bus image say "beep beep!" when you click on it.
+
+const beep = document.querySelector('.container .intro img');
+
+beep.addEventListener('click', (event) => {
+  alert('Beep beep!');
+});
