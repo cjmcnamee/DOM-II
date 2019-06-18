b077788bd17b1fe6cd4f77f4579eaa8785178859
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-06-18 15:46:56 -0400

    added more fun stuff to the fun bus

--- js/index.js
+++ js/index.js
@@ -7,3 +7,29 @@ const beep = document.querySelector('.container .intro img');
 beep.addEventListener('click', (event) => {
   alert('Beep beep!');
 });
+
+// Makes "Fun Bus" text yellow when you hover over it, and back to normal when you hover off of it
+
+const funBus = document.querySelector('.logo-heading');
+
+funBus.addEventListener('mouseover', (event) => {
+  event.stopPropagation();
+  event.target.style.color = 'gold';
+})
+funBus.addEventListener('mouseout', (event) => {
+  event.stopPropagation();
+  event.target.style.color = 'black';
+})
+
+// Makes the background color change to gray and back to white when you press a key
+
+const seizure = document.querySelector('body');
+
+seizure.addEventListener('keydown', (event) => {
+    event.target.style.backgroundColor = 'slategrey';
+})
+seizure.addEventListener('keyup', (event) => {
+    event.target.style.backgroundColor = 'white';
+})
+
+// 
